0b876d849aae5892466c9fb34e570931405fb33f
Renan-NO <re.office.25@gmail.com>
2020-02-28 15:38:32 +0000

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect('mongodb+srv://renan:Objetivo25@cluster0-ajdd9.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parametros:
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+
+
---
+++ backend/src/websocket.js
@@ -0,0 +1,39 @@
+const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
+
+let io;
+const connections = [];
+
+exports.setupWebSocket = (server) => {
+    io = socketio(server);
+
+    io.on('connection', socket => {
+        console.log(socket.id);
+        const { latitude, longitude, techs } = socket.handshake.query;
+
+        connections.push({
+            id: socket.id,
+            coordinates: {
+                latitude: Number(latitude),
+                longitude: Number(longitude),
+            },
+            techs: parseStringAsArray(techs),
+        });
+    });
+}
+
+// Faz a verificação se existem devs com a msm condição ao redor de 10 km.
+
+exports.findConnections = (coordinates, techs) => {
+    return connections.filter(connection => {
+        return calculateDistance(coordinates, connection.coordinates) < 10
+        && connection.techs.some(item => techs.includes(item))
+    })
+}
+
+exports.sendMessage = (to, message, data) => {
+    to.forEach(connection => {
+        io.to(connection.id).emit(message, data);
+    })
+}
